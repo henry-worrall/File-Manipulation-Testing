f4db32f7a6c083780a43e8e2260cf01598dd18d0
Henry Worrall <61287647+henry-worrall@users.noreply.github.com>
2020-08-22 00:42:27 +0100

    Implementing functionality

---
+++ main.py
@@ -0,0 +1,11 @@
+import os.path
+
+def move_file1_2():
+    if True==True:
+        return os.rename('folder_1/some_text.txt', 'folder_2/some_text.txt')
+
+def move_file2_1():
+    if True==True:
+        return os.rename('folder_2/some_text.txt', 'folder_1/some_text.txt')
+
+move_file1_2()
